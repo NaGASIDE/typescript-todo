bc04dc42558fbe9ab772dbb649b99d9d276b1df6
Bekzat Ashke <bekzatashken@gmail.com>
2021-06-17 18:05:12 +0600

    commit

--- src/store/Todo/reducer.ts
+++ src/store/Todo/reducer.ts
@@ -3,18 +3,23 @@ import { TodoAction, TodoActionTypes, TodosState  } from '../../types/todo'
 const initialState: TodosState = {
   todos: [],
   completed: [],
+  error: null
 }
 
 export const TodoReducer = (state = initialState, action: TodoAction): TodosState => {
   switch (action.type) {
     case TodoActionTypes.FETCH_TODOS:
-      return {todos:[], completed: []}
+      return {todos:[], completed: [], error: null}
+    case TodoActionTypes.FETCH_TODOS_ERROR:
+      return {todos:[], completed: [], error: action.payload}
+    case TodoActionTypes.FETCH_TODOS_SUCCESS:
+      return {todos:[], completed: [], error: null}
     case TodoActionTypes.ADD_TODO:
-      return {todos:[], completed: []}
+      return {todos:[], completed: [], error: null}
     case TodoActionTypes.DELETE_TODO:
-      return {todos:[], completed: []}
+      return {todos:[], completed: [], error: null}
     case TodoActionTypes.EDIT_TODO:
-      return {todos:[], completed: []}
+      return {todos:[], completed: [], error: null}
     default:
      return state
   }
---
+++ src/store/action-creators/todo.ts
@@ -0,0 +1,17 @@
+import axios from "axios"
+import { Dispatch } from "redux"
+import { TodoAction, TodoActionTypes } from "../../types/todo"
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch<TodoAction>) => {
+     try {
+        dispatch({type: TodoActionTypes.FETCH_TODOS })
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos`)
+        dispatch({type:TodoActionTypes.FETCH_TODOS_SUCCESS, payload: response.data})
+     } catch (e) {
+        dispatch({type: TodoActionTypes.FETCH_TODOS_ERROR,
+        payload: `Произошла ошибка при загрузке задач с сервера`
+      })
+     }
+  }
+}
